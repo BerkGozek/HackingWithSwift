a987b1ec3292dd4c16a9bf98b3f8a4730eb7e205
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-07 00:15:30 +0300

    Day1 ini comm, süsser Tod

---
+++ Day1/Day1/Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  Day1App.swift
+//  Day1
+//
+//  Created by Berk Gozek on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
