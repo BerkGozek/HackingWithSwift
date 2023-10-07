40eda716d984286d4352dcc34752884a66f22f13
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-07 22:14:40 +0300

    Day3 komm susser tod

---
+++ Day3/Day3/Day3App.swift
@@ -0,0 +1,17 @@
+//
+//  Day3App.swift
+//  Day3
+//
+//  Created by Berk Gozek on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
