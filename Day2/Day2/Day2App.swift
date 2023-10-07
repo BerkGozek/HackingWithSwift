b271825a35b1decf95679aa54c8320cd26546284
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-07 18:15:57 +0300

    Day 2 Comm Süsser Tod

---
+++ Day2/Day2/Day2App.swift
@@ -0,0 +1,17 @@
+//
+//  Day2App.swift
+//  Day2
+//
+//  Created by Berk Gozek on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
