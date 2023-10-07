7025908d29c8b7b6356b370877bcbfb4ad118c7a
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-07 16:13:38 +0300

    projFinnish

--- Day1/Day1/ContentView.swift
+++ Day1/Day1/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Rad: Int = 100
+    @State private var Tex: String = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                Text("Enter Radius of Circle: ")
+                TextField("Radius", value: $Rad, formatter: NumberFormatter())
+            }
+            ZStack{
+                Circle()
+                    .frame(width: CGFloat(Rad) * 2, height: CGFloat(Rad) * 2 , alignment: .center)
+                    .overlay(
+                        Text(Tex)
+                            .font(.system(size: 24))
+                            .foregroundColor(.white))
+                    .mask(
+                        Circle()
+                        .frame(width: CGFloat(Rad) * 2, height: CGFloat(Rad) * 2 , alignment: .center)
+                        )
+            }
+            
+                
+                
+            HStack{
+                Text("Enter Text inside of Circle: ")
+                TextField("Text", text: $Tex)
+            }
         }
         .padding()
+        
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
