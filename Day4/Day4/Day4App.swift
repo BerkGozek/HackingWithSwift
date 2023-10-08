378a2c02d34f2d482c42e7800f468439d367e2dd
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-08 22:34:53 +0300

    Day4 KommSusserTodd

---
+++ Day4/Day4/Day4App.swift
@@ -0,0 +1,17 @@
+//
+//  Day4App.swift
+//  Day4
+//
+//  Created by Berk Gozek on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
